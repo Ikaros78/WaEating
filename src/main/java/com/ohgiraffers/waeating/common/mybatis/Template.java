5eeddc21c4abbf33379bd37a0fe1dd21253a00fb
Ikaros78 <mydowson@gmail.com>
2022-07-19 09:42:41 +0900

    common template 추가

---
+++ src/main/java/com/ohgiraffers/waeating/common/mybatis/Template.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.waeating.common.mybatis;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.ohgiraffers.waeating.common.config.ConfigLocation;
+
+public class Template {
+	
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	public static SqlSession getSqlSession() {
+		
+		if(sqlSessionFactory == null) {
+			String resource = ConfigLocation.mybatisConfigLocation;
+			try {
+				InputStream inputStream = Resources.getResourceAsStream(resource);
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession(false);
+		
+		return sqlSession;
+	}
+	
+	
+
+}
