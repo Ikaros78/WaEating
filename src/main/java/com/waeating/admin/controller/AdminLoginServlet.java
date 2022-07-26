b4365e0dedf28e9ae55ce1c7662234e7620f429a
minjeong <cc114728@gmail.com>
2022-07-26 09:55:42 +0900

    커밋

--- src/main/java/com/waeating/admin/controller/AdminLoginServlet.java
+++ src/main/java/com/waeating/admin/controller/AdminLoginServlet.java
@@ -18,50 +18,6 @@ import com.waeating.member.model.dto.MemberDTO;
 @WebServlet("/admin/login")
 public class AdminLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-<<<<<<< HEAD
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session = request.getSession();
-				
-		request.getRequestDispatcher("/WEB-INF/views/admin/login.jsp").forward(request, response);
-	}
-
-
-
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session = request.getSession();
-		
-		String adminId = request.getParameter("id");
-		String pwd = request.getParameter("password");
-		String memberType = (String) session.getAttribute("memberType");
-		
-		System.out.println("adminId : " + adminId);
-		System.out.println("password : " + pwd);
-		System.out.println("memberTye : " + memberType);
-		
-		MemberDTO requestMember = new MemberDTO();
-		requestMember.setId(adminId);
-		requestMember.setPwd(pwd);
-		requestMember.setMemberType(memberType);
-		
-		AdminService adminService = new AdminService();
-		
-		MemberDTO loginMember = adminService.loginCheck(requestMember);
-		System.out.println(loginMember);
-		
-		if(loginMember != null) {
-			
-			session.setAttribute("loginMember", loginMember);
-			
-			response.sendRedirect(request.getContextPath());
-		} else {
-			
-			request.setAttribute("message", "로그인 실패");
-			request.getRequestDispatcher("/WEB-INF/views/common/failed.jsp").forward(request, response);
-=======
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -107,9 +63,6 @@ public class AdminLoginServlet extends HttpServlet {
 			request.getRequestDispatcher("/WEB-INF/views/common/failed.jsp").forward(request, response);
 
 
-
-	
->>>>>>> refs/remotes/origin/master
 		}
 	}
 
