6a6c35f61877f21c462a3265399ad2d9b8a59f8c
yebean <yevin39@gmail.com>
2022-07-26 09:48:01 +0900

    commit

---
+++ src/main/java/com/waeating/user/login/controller/UserFindPasswordEmailServlet.java
@@ -0,0 +1,21 @@
+package com.waeating.user.login.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserFindIdEmailServlet
+ */
+@WebServlet("/member/user/findpw/email")
+public class UserFindPasswordEmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
