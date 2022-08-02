4ed19dee33cb91ca6fde5fa994b6092b1f301826
minjeong <cc114728@gmail.com>
2022-08-02 10:56:23 +0900

    dto 수정

--- src/main/java/com/waeating/member/model/dto/WishlistDTO.java
+++ src/main/java/com/waeating/member/model/dto/WishlistDTO.java
@@ -5,6 +5,33 @@ public class WishlistDTO {
 	private String memberId;
 	private int comNo;
 	
+	public WishlistDTO() {}
+
+	public WishlistDTO(String memberId, int comNo) {
+		this.memberId = memberId;
+		this.comNo = comNo;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public int getComNo() {
+		return comNo;
+	}
+
+	public void setComNo(int comNo) {
+		this.comNo = comNo;
+	}
+
+	@Override
+	public String toString() {
+		return "WishlistDTO [memberId=" + memberId + ", comNo=" + comNo + "]";
+	}
 	
 	
 }
--- src/main/java/com/waeating/waitingRecord/model/dto/WaitingRefuseDTO.java
+++ src/main/java/com/waeating/waitingRecord/model/dto/WaitingRefuseDTO.java
@@ -5,4 +5,40 @@ public class WaitingRefuseDTO {
 	private int recordNo;
 	private String refuseReason;
 	
+	
+	public WaitingRefuseDTO() {}
+
+
+	public WaitingRefuseDTO(int recordNo, String refuseReason) {
+		this.recordNo = recordNo;
+		this.refuseReason = refuseReason;
+	}
+
+
+	public int getRecordNo() {
+		return recordNo;
+	}
+
+
+	public void setRecordNo(int recordNo) {
+		this.recordNo = recordNo;
+	}
+
+
+	public String getRefuseReason() {
+		return refuseReason;
+	}
+
+
+	public void setRefuseReason(String refuseReason) {
+		this.refuseReason = refuseReason;
+	}
+
+
+	@Override
+	public String toString() {
+		return "WaitingRefuseDTO [recordNo=" + recordNo + ", refuseReason=" + refuseReason + "]";
+	}
+	
+	
 }
