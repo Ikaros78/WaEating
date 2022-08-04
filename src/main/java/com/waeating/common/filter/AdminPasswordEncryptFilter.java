3e6aefa7e1a2ae9bd096113f623486d3b52203f4
Ikaros78 <mydowson@gmail.com>
2022-08-04 10:08:13 +0900

    관리자 회원정보 수정 및 등록 시 비밀번호 암호화 완료

---
+++ src/main/java/com/waeating/common/filter/AdminPasswordEncryptFilter.java
@@ -0,0 +1,49 @@
+package com.waeating.common.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import com.waeating.common.wrapper.EncryptRequestWrapper;
+
+/**
+ * Servlet Filter implementation class AdminPasswordEncryptFilter
+ */
+@WebFilter("/admin/member/*")
+public class AdminPasswordEncryptFilter implements Filter {
+
+    public AdminPasswordEncryptFilter() {}
+
+	public void destroy() {}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest hrequest = (HttpServletRequest) request;
+		String uri = hrequest.getRequestURI();
+		System.out.println("uri : " + uri);
+		
+		String intent = uri.substring(uri.lastIndexOf("/"));
+		System.out.println("intent : " + intent);
+		
+		/* 로그인 요청이 아닌 경우에만 암호화를 한다.*/
+		if(!"/login".equals(intent)) {
+			
+			EncryptRequestWrapper wrapper = new EncryptRequestWrapper(hrequest);
+			
+			chain.doFilter(wrapper, response);
+		} else {
+			
+			chain.doFilter(request, response);
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
